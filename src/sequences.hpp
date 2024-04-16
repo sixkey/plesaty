b8821ea4b773d779085f1460d1fda767904a8860
Filip Kučerák <xkucerak@fi.muni.cz>
2024-04-16 13:37:06 +0200

    luby: add constant, remove sandbox

--- src/sequences.hpp
+++ src/sequences.hpp
@@ -2,27 +2,35 @@
 
 #include <cassert>
 
-struct luby 
+struct luby
 {
-    unsigned int iteration = 2; 
-    unsigned int current = 1;
-    unsigned int acc = 1;
 
-    int next() 
+    unsigned int iteration = 2;
+    unsigned int current = 1;
+    unsigned int acc;
+    unsigned int base_value;
+
+    luby( unsigned int base_value )
+        : base_value( base_value )
+        , acc( base_value )
+    {
+    }
+
+    int next()
     {
         if ( current == 0 )
         {
             current++;
-            return 1;
+            return base_value;
         }
-        current++; 
+        current++;
         acc *= 2;
         if ( current >= iteration )
         {
             current = 0;
-            acc = 1;
+            acc = base_value;
             iteration++;
-        } 
+        }
         return acc;
     }
 };
